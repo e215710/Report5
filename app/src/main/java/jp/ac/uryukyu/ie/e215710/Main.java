91ecbfa2676e1e07479dbc5ad2ef485f3521857c
Fukamura Megu <e215710@ie.u-ryukyu.ac.jp>
2021-12-07 22:35:33 +0900

    ステップ1

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215710/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e215710;
+
+public class Main {
+    public static void printLength(String str) {
+        int i = str.length() ;
+        System.out.println(i) ;
+    }
+
+    public static void main(String[]args) {
+        String str = null ;
+        printLength(str);
+    }
+}
