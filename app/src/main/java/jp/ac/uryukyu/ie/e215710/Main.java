9051a4774c9a034c9528ea81904c0f0faeac1a2c
Fukamura Megu <e215710@ie.u-ryukyu.ac.jp>
2021-12-08 15:15:22 +0900

    ステップ2

--- app/src/main/java/jp/ac/uryukyu/ie/e215710/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215710/Main.java
@@ -7,7 +7,13 @@ public class Main {
     }
 
     public static void main(String[]args) {
-        String str = null ;
-        printLength(str);
+        try {
+            String str = null ;
+            printLength(str); 
+        }
+        catch(NullPointerException ex) {
+            System.out.println("エラー：nullだよ" ) ;
+            System.out.println(ex.getMessage()) ;
+        }
     }
 }
