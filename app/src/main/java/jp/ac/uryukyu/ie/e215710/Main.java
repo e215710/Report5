b042271de7a977ac3d05d2581b7642449c2d2953
Fukamura Megu <e215710@ie.u-ryukyu.ac.jp>
2021-12-09 18:15:14 +0900

    ステップ3

--- app/src/main/java/jp/ac/uryukyu/ie/e215710/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215710/Main.java
@@ -1,19 +1,30 @@
 package jp.ac.uryukyu.ie.e215710;
 
 public class Main {
+    /*
     public static void printLength(String str) {
         int i = str.length() ;
         System.out.println(i) ;
     }
+    */
+
+    public static void printChange(String str) {
+        int j = Integer.parseInt(str) ;
+        System.out.println(j) ;
+    }
 
     public static void main(String[]args) {
-        try {
+        /*try {
             String str = null ;
             printLength(str); 
         }
         catch(NullPointerException ex) {
-            System.out.println("エラー：nullだよ" ) ;
+            System.out.println("エラー : nullだよ") ;
             System.out.println(ex.getMessage()) ;
         }
-    }
+        */
+        
+        String str = "百二十三" ;
+        printChange(str);
+}
 }
